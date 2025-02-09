a3afbb1b8e6476e061b787e3ce66f2ace5be6317
habijm <habijiyan111@gmail.com>
2025-02-09 23:38:53 +0700

    update ui category

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:calendar_appbar/calendar_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:moneytracker/pages/category_page.dart';
 import 'package:moneytracker/pages/home_page.dart';
 
@@ -11,33 +12,67 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final List<Widget> _children = [
+    HomePage(),
+    CategoryPage(),
+  ];
+  int currentIndex = 0;
+
+  void onTabTapped(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CalendarAppBar(
-        accent: Colors.green,
-        backButton: false,
-        locale: 'id',
-        onDateChanged: (value) => print(value),
-        firstDate: DateTime.now().subtract(Duration(days: 140)),
-        lastDate: DateTime.now(),
-      ),
-      floatingActionButton: FloatingActionButton(
+      appBar: (currentIndex == 0)
+          ? CalendarAppBar(
+              accent: Colors.green,
+              backButton: false,
+              locale: 'id',
+              onDateChanged: (value) => print(value),
+              firstDate: DateTime.now().subtract(Duration(days: 140)),
+              lastDate: DateTime.now(),
+            )
+          : PreferredSize(
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 36, horizontal: 16),
+                  child: Text('Categories', style: GoogleFonts.montserrat(fontSize: 20),),
+                )),
+              preferredSize: Size.fromHeight(100),
+            ),
+      floatingActionButton: Visibility(
+        visible: currentIndex == 0 ? true : false, 
+        child: FloatingActionButton(
           onPressed: () {},
           backgroundColor: Colors.green,
-          child: Icon(Icons.add, color: Colors.white),),
-      body: HomePage(),
+          child: Icon(Icons.add, color: Colors.white),
+        ),
+      ),
+      body: _children[currentIndex],
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.home)),
-          SizedBox(
-            width: 20,
-          ),
-          IconButton(onPressed: (){}, icon: Icon(Icons.list)),
-        ],),
+            IconButton(
+                onPressed: () {
+                  onTabTapped(0);
+                },
+                icon: Icon(Icons.home)),
+            SizedBox(
+              width: 20,
+            ),
+            IconButton(
+                onPressed: () {
+                  onTabTapped(1);
+                },
+                icon: Icon(Icons.list)),
+          ],
+        ),
       ),
     );
   }
